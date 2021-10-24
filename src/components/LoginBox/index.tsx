8dbd109005dd5ff31fdbd8d1af92e5863da563e6
Pablo Gomes <pablojr17@gmail.com>
2021-10-24 00:04:54 -0300

    🚀 Stage 2

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,5 +1,19 @@
+import { useContext } from 'react';
+import { VscGithubInverted } from 'react-icons/vsc'
+import { AuthContext } from '../../contexts/auth';
+
+import styles from './styles.module.scss';
+
 export function LoginBox() {
+  const { signInUrl } = useContext(AuthContext);
+
   return (
-    <h1>LoginBox</h1>
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com Github
+      </a>
+    </div>
   )
 }
